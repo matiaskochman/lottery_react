4f2974e1f72b602c6f9402327d7e2c5e2a6ef8cd
matias <matiaskochman@gmail.com>
2018-03-16 12:25:27 -0300

    first commit

---
+++ src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from "./web3";
+
+const contract_address = "0x0E3Ba8eF93E1ee13bA4332EBd64DDd117EB36837";
+
+const contract_abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+//exports a copy of the contract that exists in the blockchain
+export default new web3.Contract(abi,address);
